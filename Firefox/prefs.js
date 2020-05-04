d28258b409fc908588923ada865329957d00c53f
aantu014 <50641330+aantu014@users.noreply.github.com>
2020-05-03 23:32:28 -0400

    Update prefs.js

--- Firefox/prefs.js
+++ Firefox/prefs.js
@@ -104,6 +104,8 @@ user_pref("geo.enabled", false);
 user_pref("layers.mlgpu.sanity-test-failed", false);
 user_pref("layout.spellcheckDefault", 0);
 user_pref("media.autoplay.default", 0);
+//user_pref("media.eme.enabled", false);  //Wont be able to watch watch Netflix nor Crunchyroll
+//user_pref("media.gmp-widevinecdm.enabled", false); //Wont be able to watch watch Netflix nor Crunchyroll
 user_pref("media.gmp-gmpopenh264.enabled", false);
 user_pref("media.gmp.storage.version.observed", 1);
 user_pref("media.hardware-video-decoding.failed", false);
