be5bbc19c31d4d9e742bc9f7391a2d2d0afcef08
aantu014 <50641330+aantu014@users.noreply.github.com>
2020-05-08 16:08:38 -0400

    Update user.js

--- Palemoon/user.js
+++ Palemoon/user.js
@@ -165,3 +165,5 @@ user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; rv:68.0) 
 user_pref("browser.geolocation.warning.infoURL", "");
 user_pref("layers.shared-buffer-provider.enabled", false);
 user_pref("dom.workers.enabled", false);
+user_pref("dom.indexedDB.logging.details", false);
+user_pref("dom.indexedDB.logging.enabled", false);
