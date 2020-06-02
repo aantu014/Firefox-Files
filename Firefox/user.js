b6d25f7ad6959efdcd125a086c016b7d573d31e0
aantu014 <50641330+aantu014@users.noreply.github.com>
2020-06-02 17:54:49 -0400

    Update user.js

--- Firefox/user.js
+++ Firefox/user.js
@@ -223,7 +223,7 @@ user_pref("permissions.default.geo", 2);
 user_pref("permissions.default.microphone", 2);
 user_pref("permissions.delegation.enabled", false);
 user_pref("places.history.enabled", false);
-user_pref("plugin.mousewheel.enabled", false);
+//user_pref("plugin.mousewheel.enabled", false); Windows
 user_pref("plugin.scan.plid.all", false);
 user_pref("privacy.clearOnShutdown.offlineApps", true);
 user_pref("privacy.clearOnShutdown.siteSettings", true);
